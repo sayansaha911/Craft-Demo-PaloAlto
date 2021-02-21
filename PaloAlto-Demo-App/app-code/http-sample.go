48c9ee591de1667f65c67450679b2f96ad5e12ed
sayansaha911 <sayan.saha911@yahoo.com>
2021-02-21 14:11:19 -0500

    added shell script to push helm

--- PaloAlto-Demo-App/app-code/http-sample.go
+++ PaloAlto-Demo-App/app-code/http-sample.go
@@ -1,15 +1,15 @@
 package main
 
 import (
-    "fmt"
-    "net/http"
+	"fmt"
+	"net/http"
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
-    fmt.Fprintf(w, "Congratulations! Your Go application has been successfully deployed on Kubernetes.")
+	fmt.Fprintf(w, "Welcome to PaloAlto Craft Demo")
 }
 
 func main() {
-    http.HandleFunc("/", handler)
-    http.ListenAndServe(":3000", nil)
+	http.HandleFunc("/", handler)
+	http.ListenAndServe(":3000", nil)
 }
